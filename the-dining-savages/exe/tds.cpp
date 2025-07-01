b9e2db60d2b0fc9a3ad827e28c76f1a55cfb7daf
André Willyan <andrewillyan@tutanota.com>
2025-07-01 00:22:50 -0300

    feat: fazer exemplo básico utilizando monitores na CLI

--- the-dining-savages/exe/tds.cpp
+++ the-dining-savages/exe/tds.cpp
@@ -1,20 +1,17 @@
-#include <CLI11/CLI11.hpp>
 #include <chrono>
+#include <climits>
+#include <memory>
 #include <stop_token>
 #include <thread>
 
+#include <CLI11/CLI11.hpp>
+
 #include "Camp.hpp"
+#include "CookWMonitor.hpp"
+#include "Monitor.hpp"
+#include "SavageWMonitor.hpp"
 
 #define TDS_VERSION "0.1.0"
-
-void dummy_savage_func(std::stop_token st) {
-  std::cout << "Worker started working...\n";
-  std::chrono::milliseconds sleep_time{500};
-  while (!st.stop_requested()) {
-    std::this_thread::sleep_for(sleep_time);
-  }
-  std::cout << "Worker exiting cleanly\n";
-}
 
 int main(int argc, char **argv) {
   CLI::App app("The Dining Savages problem");
@@ -25,13 +22,33 @@ int main(int argc, char **argv) {
   app.add_option("-s,--savages", n_savages, "Number of Savage threads")
       ->required(true);
 
+  int max_serving{0};
+  app.add_option("-m,--max-serving", max_serving, "Max capacity of the pot")
+      ->required(true)
+      ->check(CLI::Range(1, INT_MAX));
+
   CLI11_PARSE(app, argc, argv);
 
-  Camp camp{n_savages, dummy_savage_func};
-  for (int i = 0; i < 10; ++i) {
-    camp.task();
-    std::this_thread::sleep_for(std::chrono::milliseconds{100});
-  }
+  // state variables
+  std::shared_ptr<TDS::Monitor> monitor = std::make_shared<TDS::Monitor>();
+  std::shared_ptr<TDS::ConditionVariable> empty_cv =
+      std::make_shared<TDS::ConditionVariable>();
+  std::shared_ptr<TDS::ConditionVariable> full_cv =
+      std::make_shared<TDS::ConditionVariable>();
+  std::shared_ptr<std::atomic_uint> servings =
+      std::make_shared<std::atomic_uint>(0);
+
+  // thread functions
+  auto savage_func = [&](std::stop_token st) {
+    TDS::SavageWMonitor savage{monitor, empty_cv, full_cv, servings};
+    savage(st);
+  };
+  TDS::CookWMonitor cook{monitor, empty_cv, full_cv, servings,
+                         static_cast<uint>(max_serving)};
+
+  // camp thread manager
+  TDS::Camp camp{n_savages, savage_func, cook};
+  std::this_thread::sleep_for(std::chrono::milliseconds{500});
   camp.stop_all();
 
   return 0;
